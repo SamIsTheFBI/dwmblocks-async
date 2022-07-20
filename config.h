6952af1486d78303bacf5a5edba6244e9a25809f
Shivam Shekhar Soy <70562711+SamIsTheFBI@users.noreply.github.com>
2022-07-20 09:48:13 +0530

    adding my configurations

--- config.h
+++ config.h
@@ -1,16 +1,11 @@
-#define CMDLENGTH 45
-#define DELIMITER "  "
+#define CMDLENGTH 15000
+#define DELIMITER " "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("~/.local/bin/now-playing sbar",   1,    1),
+	BLOCK("~/.local/bin/nettraf sbar",   1,    2),
+	BLOCK("~/.local/bin/sys-prop",   1,    3),
+	// BLOCK("~/.local/bin/battery sbar", 5,    5),
+	BLOCK("~/.local/bin/date-time",   1,    4),
 };
